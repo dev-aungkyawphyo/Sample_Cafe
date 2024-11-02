a8500238efc4c564f35812c7e6d56566601158ce
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-11-03 02:57:21 +0630

    first commit

--- Sample_Cafe/Helpers/Storyboarded.swift
+++ Sample_Cafe/Helpers/Storyboarded.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+enum Storyboard: String {
+    case login = "LoginScreen"
+    
+    var name: String {
+        self.rawValue
+    }
+}
+
+protocol Storyboarded {
+    static func instantiate(storyboard: Storyboard) -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    static func instantiate(storyboard: Storyboard) -> Self {
+        let storyboard = UIStoryboard(name: storyboard.name, bundle: Bundle.main)
+        return storyboard.instantiateViewController(offType: Self.self)
+    }
+}
+
+extension UIStoryboard {
+    func instantiateViewController<T: UIViewController>(offType _: T.Type, withIdentifier identifier: String? = nil) -> T {
+        let identifier = identifier ?? String(describing: T.self)
+        return instantiateViewController(withIdentifier: identifier) as! T
+    }
+}
--- Sample_Cafe/Mdoules/LoginScreenModule/LoginScreenViewController.swift
+++ Sample_Cafe/Mdoules/LoginScreenModule/LoginScreenViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LoginScreenViewController: UIViewController {
+class LoginScreenViewController: SampleViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
